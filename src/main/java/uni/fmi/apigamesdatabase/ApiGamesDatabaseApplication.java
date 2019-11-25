d8578488cbf274265310ba5f1d689c4bad72f22c
koko1313 <kaloyanvellichkov@gmail.com>
2019-11-25 12:03:37 +0200

    initialize project

---
+++ src/main/java/uni/fmi/apigamesdatabase/ApiGamesDatabaseApplication.java
@@ -0,0 +1,13 @@
+package uni.fmi.apigamesdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGamesDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGamesDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uni/fmi/apigamesdatabase/ApiGamesDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.fmi.apigamesdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGamesDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
