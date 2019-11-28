0dac25729e51755db0e379627a104965f21b5767
koko1313 <kaloyanvellichkov@gmail.com>
2019-11-28 19:50:43 +0200

    scripts separated in different files

--- src/main/webapp/assets/js/requests.js
+++ src/main/webapp/assets/js/requests.js
@@ -8,29 +8,3 @@ function ajax(method, url, callback) {
         callback(resp);
     });
 }
-
-/*
-function getAllGames() {
-    ajax("GET", "/game/all", function(games) {
-        return games;
-    });
-}
-
-function getAllGenres() {
-    ajax("GET", "/genre/all", function(genres) {
-        return genres;
-    });
-}
-
-function getAllPlatforms() {
-    ajax("GET", "platform/all", function(platforms) {
-        console.log(platforms);
-    });
-}
-
-function getAllDevelopers() {
-    ajax("GET", "developer/all", function(developers) {
-        console.log(developers);
-    });
-}
-*/
